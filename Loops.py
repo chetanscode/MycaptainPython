3cca0205d7b158b3552ebf484fc6a5f7aec03fe3
chetanscode <146327753+chetanscode@users.noreply.github.com>
2024-02-10 17:32:46 +0530

    Fibobanci Numbers -  Loops.py

--- Loops.py
+++ Loops.py
@@ -4,6 +4,6 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
-# Print the first 100 Fibonacci numbers
-for i in range(100):
+# Print the first 15 Fibonacci numbers
+for i in range(15):
     print(fibonacci(i))
