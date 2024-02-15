d51391b30d9366d7fced2fe845d76a1f8750deb6
chetanscode <146327753+chetanscode@users.noreply.github.com>
2024-02-15 14:03:26 +0530

    Update motor.py

--- motor.py
+++ motor.py
@@ -26,3 +26,69 @@ if(Serial.available() > 0){
    }
  } 
 }
+
+
+void forward()
+{
+ motor1.setSpeed(255); //Define maximum velocity
+ motor1.run(FORWARD); //rotate the motor clockwise
+ motor2.setSpeed(255); //Define maximum velocity
+ motor2.run(FORWARD); //rotate the motor clockwise
+ 
+ motor3.setSpeed(255); //Define maximum velocity
+ motor3.run(FORWARD); //rotate the motor clockwise
+ motor4.setSpeed(255); //Define maximum velocity
+ motor4.run(FORWARD); //rotate the motor clockwise
+}
+
+void back()
+{
+ motor1.setSpeed(255); //Define maximum velocity
+ motor1.run(FORWARD); //rotate the motor clockwise
+ motor2.setSpeed(255); //Define maximum velocity
+ motor2.run(FORWARD); //rotate the motor clockwise
+ 
+ motor3.setSpeed(255); //Define maximum velocity
+ motor3.run(FORWARD); //rotate the motor clockwise
+ motor4.setSpeed(255); //Define maximum velocity
+ motor4.run(FORWARD); //rotate the motor clockwise
+}
+
+void left()
+{
+ motor1.setSpeed(255); //Define maximum velocity
+ motor1.run(FORWARD); //rotate the motor clockwise
+ motor2.setSpeed(255); //Define maximum velocity
+ motor2.run(FORWARD); //rotate the motor clockwise
+ 
+ motor3.setSpeed(255); //Define maximum velocity
+ motor3.run(FORWARD); //rotate the motor clockwise
+ motor4.setSpeed(255); //Define maximum velocity
+ motor4.run(FORWARD); //rotate the motor clockwise
+}
+
+void right()
+{
+ motor1.setSpeed(255); //Define maximum velocity
+ motor1.run(FORWARD); //rotate the motor clockwise
+ motor2.setSpeed(255); //Define maximum velocity
+ motor2.run(FORWARD); //rotate the motor clockwise
+ 
+ motor3.setSpeed(255); //Define maximum velocity
+ motor3.run(FORWARD); //rotate the motor clockwise
+ motor4.setSpeed(255); //Define maximum velocity
+ motor4.run(FORWARD); //rotate the motor clockwise
+}
+
+void stop()
+{
+ motor1.setSpeed(255); //Define maximum velocity
+ motor1.run(FORWARD); //rotate the motor clockwise
+ motor2.setSpeed(255); //Define maximum velocity
+ motor2.run(FORWARD); //rotate the motor clockwise
+ 
+ motor3.setSpeed(255); //Define maximum velocity
+ motor3.run(FORWARD); //rotate the motor clockwise
+ motor4.setSpeed(255); //Define maximum velocity
+ motor4.run(FORWARD); //rotate the motor clockwise
+}
